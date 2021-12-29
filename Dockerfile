f848052a594ddafc78d383068f4e8156f419812e
Brian Leung <brianleung@brianmm.localdomain>
2021-12-29 15:17:33 -0800

    Functional auth

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,16 @@ FROM python:3.10
 
 RUN curl -sSL https://install.python-poetry.org | python3 -
 
+
 WORKDIR /gmail-cleaner
 
 COPY . .
 
 RUN /root/.local/bin/poetry install
 
-CMD /root/.local/bin/poetry shell
+
+EXPOSE 8080
+
+CMD /root/.local/bin/poetry run python gmail_cleaner/main.py
+
+# CMD /root/.local/bin/poetry run python -m http.server 8080 
---
+++ gmail_cleaner/main.py
@@ -0,0 +1,67 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+# SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://mail.google.com/']
+
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=8080, open_browser=False)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        # Call the Gmail API
+        gmail_service = build('gmail', 'v1', credentials=creds)
+
+        results = gmail_service.users().threads().list(
+            userId='me', 
+            q='Gifting complete. Now, treat yourself.',
+            maxResults=100,
+        ).execute()
+
+        threads = results.get('threads', [])
+
+        if not threads:
+            print('No threads found.')
+            return
+
+        for t in threads:
+            thread = gmail_service.users().threads().get(userId='me', id=t['id']).execute()
+            print(len(thread['messages']))
+            msg = thread['messages'][0]
+            print(msg.keys())
+
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f'An error occurred: {error}')
+
+
+if __name__ == '__main__':
+    main()
